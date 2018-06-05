0531f42cda9abc93520175ece5de301647351f04
Trung Hai Nguyen <nguyentrungh@gmail.com>
2018-06-04 21:12:44 -0500

    modify calculate_receptor_grid.py

--- protein_ligand_scripts/calculate_receptor_grid.py
+++ protein_ligand_scripts/calculate_receptor_grid.py
@@ -1,10 +1,11 @@
 """
 to calculate receptor grids
 """
+from __future__ import print_function
+
 import sys
 import argparse
 
-import netCDF4 as nc
 
 # change this 
 sys.path.append("/home/tnguye46/opt/src/BPMFwFFT/bpmfwfft")
@@ -25,14 +26,14 @@ args = parser.parse_args()
 
 if not is_nc_grid_good(args.grid_nc_out):
 
-    potential_grid = RecGrid(args.receptor_prmtop, args.lj_scale_factor, 
-                            args.receptor_inpcrd, args.bsite,
-                            args.grid_nc_out,
-                            new_calculation=True,
-                            spacing=args.spacing)
+    potential_grid = RecGrid(args.receptor_prmtop, args.lj_scale_factor,
+                             args.receptor_inpcrd, args.bsite,
+                             args.grid_nc_out,
+                             new_calculation=True,
+                             spacing=args.spacing)
 
     potential_grid.write_pdb(args.pdb_out, "w")
     potential_grid.write_box(args.enclosing_box_out)
 
 else:
-    print args.grid_nc_out + " exists and is good. So nothing to be done!"
+    print(args.grid_nc_out + " exists and is good. So nothing to be done!")
