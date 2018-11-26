02a8dfa65f87db752584e12274e371b1649d70d2
Trung Hai Nguyen <nguyentrungh@gmail.com>
2018-11-26 10:43:04 -0600

    modify run_download_pdb.py

--- protein_protein_scripts/run_download_pdb.py
+++ protein_protein_scripts/run_download_pdb.py
@@ -1,30 +1,11 @@
 """
 run this script to download all pdb listed in the affinity benchmark set
 """
-
-from __future__ import print_function
-
-import urllib
 import argparse
 import os
 
-from _affinity_data import AffinityData
-
-PDB_URL = "http://www.rcsb.org/pdb/files"
-NOT_AVAIL_MESSAGE = "the requested file is not available"
-
-def download_coord(id):
-    """
-    download pdb with id and save it with id.pdb (lower case)
-    """
-    pdb_file = id.lower() + ".pdb"
-    url = os.path.join(PDB_URL, pdb_file)
-    data = urllib.urlopen(url).read()
-    if NOT_AVAIL_MESSAGE in data:
-        raise RuntimeError(data)
-    open(pdb_file, "w").write(data)
-    return None
-
+from _affinity_data import affinity_data
+from _pdb import download_coord
 
 AFFINITY_DATA_FILES = ["affinity_v1.tsv",  "affinity_v2.tsv"]
 
@@ -35,12 +16,12 @@ args = parser.parse_args()
 
 affinity_data_files = [os.path.join(args.affinity_data_dir, file) for file in AFFINITY_DATA_FILES]
 
-aff_data = AffinityData(affinity_data_files)
+aff_data = affinity_data(affinity_data_files)
 pdb_ids = aff_data.unique_pdb_ids()
 
 for id in pdb_ids:
-    print("downloading " + id)
+    print "downloading " + id
     download_coord(id)
 
-print("%d files downloaded"%len(pdb_ids))
-print("Done")
+print "%d files downloaded"%len(pdb_ids)
+print "Done"
