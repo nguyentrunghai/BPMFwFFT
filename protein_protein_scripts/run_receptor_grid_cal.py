005d5bdd2a24d31d49b5f01e072b06b4efed3029
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-20 17:14:30 +0700

    clean up

--- protein_protein_scripts/run_receptor_grid_cal.py
+++ protein_protein_scripts/run_receptor_grid_cal.py
@@ -1,5 +1,8 @@
 """
+run receptor grid calculation
 """
+
+from __future__ import print_function
 
 import os
 import sys
@@ -9,26 +12,27 @@ import argparse
 from _receptor_grid_cal import rec_grid_cal, is_nc_grid_good, get_grid_size_from_lig_rec_crd
 
 parser = argparse.ArgumentParser()
-parser.add_argument( "--max_jobs",      type=int, default = 100)
-parser.add_argument( "--amber_dir",     type=str, default = "amber")
-parser.add_argument( "--coord_dir",     type=str, default = "min")
-parser.add_argument( "--out_dir",     type=str, default = "out")
+parser.add_argument("--max_jobs",      type=int, default=100)
+parser.add_argument("--amber_dir",     type=str, default="amber")
+parser.add_argument("--coord_dir",     type=str, default="min")
+parser.add_argument("--out_dir",     type=str, default="out")
 
-parser.add_argument( "--lj_scale",    type=float, default = 0.6)
-parser.add_argument( "--spacing",     type=float, default = 0.5)
-parser.add_argument( "--buffer",      type=float, default = 1.0)
+parser.add_argument("--lj_scale",    type=float, default=0.6)
+parser.add_argument("--spacing",     type=float, default=0.5)
+parser.add_argument("--buffer",      type=float, default=1.0)
 
-parser.add_argument( "--submit",   action="store_true", default=False)
+parser.add_argument("--submit",   action="store_true", default=False)
 
 args = parser.parse_args()
 
-LIGAND_INPCRD   = "ligand.inpcrd"
+LIGAND_INPCRD = "ligand.inpcrd"
 RECEPTOR_INPCRD = "receptor.inpcrd"
 RECEPTOR_PRMTOP = "receptor.prmtop"
 
 GRID_NC = "grid.nc"
 PDB_OUT = "receptor_trans.pdb"
 BOX_OUT = "box.pdb"
+
 
 def is_running(qsub_file, log_file, nc_file):
     if os.path.exists(qsub_file) and os.path.exists(nc_file) and (not os.path.exists(log_file)):
@@ -53,15 +57,15 @@ if args.submit:
         lig_inpcrd = os.path.join(coord_dir, complex, LIGAND_INPCRD)
         box_sizes[complex] = get_grid_size_from_lig_rec_crd(rec_inpcrd, lig_inpcrd, args.buffer)
     complex_names.sort(key=lambda name: box_sizes[name])
-    print "Complex    box size"
+    print("Complex    box size")
     for c in complex_names:
-        print c, box_sizes[c]
+        print(c, box_sizes[c])
 
     if args.max_jobs > 0:
         max_jobs = args.max_jobs
     else:
         max_jobs = len(complex_names)
-    print "max_jobs = %d"%max_jobs
+    print("max_jobs = %d"%max_jobs)
 
     job_count = 0
     for complex in complex_names:
@@ -70,13 +74,13 @@ if args.submit:
             os.makedirs(complex)
 
         id = complex[:4].lower()
-        amber_sub_dir  = os.path.join(amber_dir, complex)
-        coor_sub_dir   = os.path.join(coord_dir, complex) 
+        amber_sub_dir = os.path.join(amber_dir, complex)
+        coor_sub_dir = os.path.join(coord_dir, complex)
 
         out_dir = os.path.abspath(complex)
 
         qsub_file = os.path.join(out_dir, id+"_grid.job")
-        log_file  = os.path.join(out_dir, id+"_grid.log")
+        log_file = os.path.join(out_dir, id+"_grid.log")
         qsub_script = '''#!/bin/bash
 #PBS -S /bin/bash
 #PBS -o %s '''%log_file + '''
@@ -95,28 +99,28 @@ python ''' + this_script + \
 
         if not is_nc_grid_good(os.path.join(out_dir, GRID_NC)) and not is_running(qsub_file, log_file, 
                                                                 os.path.join(out_dir, GRID_NC)): 
-            print "Submitting %s"%complex
+            print("Submitting %s"%complex)
             open(qsub_file, "w").write(qsub_script)
             os.system("qsub %s" %qsub_file)
             job_count += 1
             if job_count == max_jobs:
-                print "Max number of jobs %d reached."%job_count
+                print("Max number of jobs %d reached."%job_count)
                 break
         else:
-            print "Calculation for %s is done"%complex
+            print("Calculation for %s is done"%complex)
 
 else:
-    prmtop     = os.path.join(args.amber_dir, RECEPTOR_PRMTOP)
-    lj_scale   = args.lj_scale
+    prmtop = os.path.join(args.amber_dir, RECEPTOR_PRMTOP)
+    lj_scale = args.lj_scale
     rec_inpcrd = os.path.join(args.coord_dir, RECEPTOR_INPCRD)
     lig_inpcrd = os.path.join(args.coord_dir, LIGAND_INPCRD)
 
-    spacing    = args.spacing
-    buffer     = args.buffer
+    spacing = args.spacing
+    buffer = args.buffer
 
-    grid_out   = os.path.join(args.out_dir, GRID_NC)
-    pdb_out    = os.path.join(args.out_dir, PDB_OUT)
-    box_out    = os.path.join(args.out_dir, BOX_OUT)
+    grid_out = os.path.join(args.out_dir, GRID_NC)
+    pdb_out = os.path.join(args.out_dir, PDB_OUT)
+    box_out = os.path.join(args.out_dir, BOX_OUT)
 
     rec_grid_cal(prmtop, lj_scale, rec_inpcrd, lig_inpcrd, spacing, buffer, grid_out, pdb_out, box_out)
 
