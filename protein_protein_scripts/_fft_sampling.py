cab9267200de0f0e0925a7df471e17a86fc34058
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-20 19:33:47 +0700

    clean up

--- protein_protein_scripts/_fft_sampling.py
+++ protein_protein_scripts/_fft_sampling.py
@@ -1,5 +1,7 @@
 """
+functions to run FFT sampling
 """
+from __future__ import print_function
 
 import sys
 import os
@@ -7,10 +9,11 @@ import os
 import netCDF4
 import numpy as np
 
-sys.path.append("/home/tnguye46/opt/src/Bpmf_with_FFT")
-from FFT_Sampling import Sampling
+sys.path.append("/home/tnguye46/opt/src/BPMFwFFT/bpmfwfft")
+from fft_sampling import Sampling
 
 BSITE_FILE = None
+
 
 def sampling(rec_prmtop, lj_sigma_scal_fact, 
                 rec_inpcrd, grid_nc_file,
@@ -18,8 +21,6 @@ def sampling(rec_prmtop, lj_sigma_scal_fact,
                 lig_coor_nc, nr_lig_conf, start_index,
                 energy_sample_size_per_ligand,
                 output_nc):
-    """
-    """
     lig_nc_handle = netCDF4.Dataset(lig_coor_nc, "r")
     lig_coord_ensemble = lig_nc_handle.variables["positions"][start_index : start_index + nr_lig_conf]
     lig_nc_handle.close()
@@ -32,35 +33,9 @@ def sampling(rec_prmtop, lj_sigma_scal_fact,
                         temperature=300.)
 
     sampler.run_sampling()
-    print "Sampling Done"
+    print("Sampling Done")
     return None
 
-def is_sampling_nc_good_WRONG(nc_file, nr_extracted_lig_conf):
-    raise RuntimeError("Do not use")
-    if not os.path.exists(nc_file):
-        return False
-
-    nc_handle = netCDF4.Dataset(nc_file, "r")
-
-    cond1 = nc_handle.variables["rec_positions"].shape[1] == 3
-    if not cond1:
-        return False
-
-    cond2 = nc_handle.variables["lig_positions"].shape[2] == 3
-    if not cond2:
-        return False
-
-    cond3 = nc_handle.variables["lig_positions"].shape[0] == nc_handle.variables["lig_com"].shape[0] == \
-            nc_handle.variables["volume"].shape[0] == nc_handle.variables["nr_grid_points"].shape[0] == \
-            nc_handle.variables["exponential_sums"].shape[0] == nc_handle.variables["log_of_divisors"].shape[0] == \
-            nc_handle.variables["resampled_energies"].shape[0] == nc_handle.variables["resampled_trans_vectors"].shape[0]
-    if not cond3:
-        return False
-
-    cond4 = nc_handle.variables["lig_positions"].shape[0] == nr_extracted_lig_conf
-    if not cond4:
-        return False
-    return True
 
 def is_sampling_nc_good(nc_file, nr_extracted_lig_conf):
     if not os.path.exists(nc_file):
@@ -68,8 +43,9 @@ def is_sampling_nc_good(nc_file, nr_extracted_lig_conf):
 
     try:
         nc_handle = netCDF4.Dataset(nc_file, "r")
-    except RuntimeError:
-        print "HDF error in "+nc_file
+    except RuntimeError as e:
+        print(nc_file)
+        print(e)
         return True
     else:
         pass
@@ -82,6 +58,7 @@ def is_sampling_nc_good(nc_file, nr_extracted_lig_conf):
         return False
 
     return True
+
 
 def parse_nr_ligand_confs(submit_file):
     if os.path.exists(submit_file):
