9585a5e863da19f42065b916d5fbc061be8a11e3
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-20 09:02:39 +0700

    fix bug

--- protein_protein_scripts/run_download_pdb.py
+++ protein_protein_scripts/run_download_pdb.py
@@ -7,7 +7,7 @@ import argparse
 import os
 import urllib
 
-from _affinity_data import affinity_data
+from _affinity_data import AffinityData
 
 AFFINITY_DATA_FILES = ["affinity_v1.tsv",  "affinity_v2.tsv"]
 
@@ -35,7 +35,7 @@ def download_coord(id):
 
 affinity_data_files = [os.path.join(args.affinity_data_dir, file) for file in AFFINITY_DATA_FILES]
 
-aff_data = affinity_data(affinity_data_files)
+aff_data = AffinityData(affinity_data_files)
 pdb_ids = aff_data.unique_pdb_ids()
 
 for pdb_id in pdb_ids:
