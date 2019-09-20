2348943878e6c96a5e755d7a3b9f04e64369f338
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-20 20:31:38 +0700

    clean up

--- protein_protein_scripts/_postprocess.py
+++ protein_protein_scripts/_postprocess.py
@@ -1,20 +1,21 @@
 """
+define function to run postprocessing
 """
 import sys
 
-sys.path.append("/home/tnguye46/opt/src/Bpmf_with_FFT")
+sys.path.append("/home/tnguye46/opt/src/BPMFwFFT/bpmfwfft")
 from postprocess import PostProcess
 
+
 SOLVENT_PHASES = ["OpenMM_GBn", "OpenMM_GBn2", "OpenMM_HCT", "OpenMM_OBC1", "OpenMM_OBC2"]
-                    #"sander_PBSA", "sander_OBC2"]
+#SOLVENT_PHASES.extend(["sander_PBSA", "sander_OBC2"])
+
 TEMPERATURE = 300.
 
 def post_process(rec_prmtop, lig_prmtop, complex_prmtop, sampling_nc_file, 
                     nr_resampled_complexes, 
                     sander_tmp_dir, 
                     rec_pdb_out, lig_pdb_out, bpmf_pkl_out):
-    """
-    """
     post_pro = PostProcess(rec_prmtop, lig_prmtop, complex_prmtop, sampling_nc_file,
                             SOLVENT_PHASES, nr_resampled_complexes, TEMPERATURE, sander_tmp_dir)
     post_pro.write_rececptor_pdb(rec_pdb_out)
